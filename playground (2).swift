65d4e87c98995fe8a1b8271944d69b19273b42d1
JulioBarbosa1012 <79843851+JulioBarbosa1012@users.noreply.github.com>
2021-04-26 20:48:45 -0300

    Add files via upload

---
+++ playground (2).swift
@@ -0,0 +1,32 @@
+import Foundation
+
+print("Júlio Barbosa - 11900296")
+
+//1
+var curta = "Júlio"
+var longa:String = "Barbosa"
+
+//2
+let placa = "DEF-4040"
+var modelo = "Gol Bolinha"
+var portas = 4
+var marca = "Volkswagen"
+
+//3
+
+//Anotação é quando colocamos duas barras para a direita e explicamos o procedimento que estamos produzindo.
+
+//4
+let inteiro: Int = 45
+var conversor = "\(inteiro)"
+print(conversor)
+
+//5 
+
+//Type safety: Swift é uma linguagem de tipo seguro. Uma linguagem de tipo seguro incentiva você a ser 
+//claro sobre os tipos de valores com os quais seu código pode trabalhar. Se parte do seu código espera uma String,
+//você não pode passar um Int por engano.
+
+//Type Inference: Se você não especificar o tipo de valor de que precisa, o Swift usa a inferência de tipo para 
+//descobrir o tipo apropriado. A inferência de tipo permite que um compilador deduza o tipo de uma expressão específica 
+//automaticamente ao compilar seu código, simplesmente examinando os valores fornecidos.
